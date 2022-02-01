38e2f2e67e554f80993d38140d33edda1ff65892
KseniyaChepelevich <chepelevich.qamid.5@gmail.com>
2022-02-01 14:41:36 +0300

    first commit variables

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,33 @@
+'use strict'
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
+  let arr = [];
+  let x = 0;
+  let x1 = 0;
+  let x2 = 0;
+
+  let D = b ** 2 - 4 * a * c;
+  
+  if (D < 0) {
+  } else if (D === 0) {
+      x = (-b) / (2 * a)
+      arr.push(x);
+    } else {
+        x1 = (-b + Math.sqrt(D)) / (2 * a);
+        arr.push(x1);
+        x2 = (-b - Math.sqrt(D)) / ( 2 * a);
+        arr.push(x2);
+      } 
   return arr; // array
 }
+
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
 
+  
   // код для задачи №2 писать здесь
 
   return totalAmount;
 }
+

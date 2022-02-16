64ed29085688a9c290c8eb9857aeba844fd9c878
KseniyaChepelevich <chepelevich.qamid.5@gmail.com>
2022-02-16 15:07:54 +0300

    Homework to the lecture 4 objects

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,44 @@
-function Student(name, gender, age) {
-    // Ваш код
+'use strict'
 
+function Student(name, gender, age) {
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) {
+    this.marks = [mark];
+  } else {
+    this.marks.push(mark)
+  }
+}
+
+Student.prototype.addMarks = function (...marks) {
+  this.marks = []; 
+  for(let mark of marks) {
+    this.marks.push(mark);
+  }     
+}
+
+Student.prototype.getAverage = function () {
+  let sum = 0;
+  let avg = 0;
+  for(let i = 0; i < this.marks.length; i++) {
+    sum += this.marks[i];
+  }
+  avg = sum / this.marks.length;
+  this.average = avg;
+  return this.average;
+}
+
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+}
+
